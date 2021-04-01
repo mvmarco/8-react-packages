e96a236cc08672e33cb1307e43d82c7384deb2ac
marco corapi <marco.corapi@outlook.it>
2021-04-01 18:06:22 +0200

    initiated notes about packages

---
+++ src/App.js
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Packages</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// ############################
+// COMMON PACKAGES TO INSTALL
+// ############################
+
+//  npm install react-router-dom framer-motion react-intersection-observer styled-components
+
+// ############################
+// REACT-ROUTER-DOM
+// ############################
+
+// ############################
+// FRAMER-MOTION
+// ############################
+
+// ############################
+// REACT-INTERSECTION-OBSERVER
+// ############################
+
+// ############################
+// STYLED COMPONENTS
+// ############################
+

518b1d2f6a6df96d231ed5092cd6037ab3d9e253
marco corapi <marco.corapi@outlook.it>
2021-04-02 19:34:23 +0200

    styled components notes

--- src/App.js
+++ src/App.js
@@ -31,3 +31,18 @@ export default App;
 // STYLED COMPONENTS
 // ############################
 
+/* 
+  https://styled-components.com/
+
+  What style components allow us to do is to write JavaScript styles but it will look 
+  exactly like CSS properties. You could also write the style like:
+  style={{backgroundColor: red}} but with styled components you do:
+
+  const button = styled.a``
+
+  the benefit is that everything is gonna be in one page.
+
+  1.step: npm install --save styled-components
+  2: import styled from 'styled-components';           wherever you gotta need it
+
+*/
